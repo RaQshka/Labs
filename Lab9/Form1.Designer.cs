a63a9fb3ec0b58a0b7caa08478a1cba1fcb01b8c
RaQshka <buba_q20@mail.ru>
2024-11-14 18:09:20 +0300

    Created: Lab9

---
+++ Lab9/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ds = new Lab9.ds();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.regionslistBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.regions_listTableAdapter = new Lab9.dsTableAdapters.regions_listTableAdapter();
+            this.regionslistBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.regioncityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.city_listTableAdapter = new Lab9.dsTableAdapters.city_listTableAdapter();
+            this.citynameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.streetcityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.street_listTableAdapter = new Lab9.dsTableAdapters.street_listTableAdapter();
+            this.streetnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ds)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.regionslistBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.regionslistBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.regioncityBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.streetcityBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label3);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.dataGridView2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.dataGridView1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.comboBox1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(467, 419);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(467, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Улицы: ";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.streetnameDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.streetcityBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(15, 261);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(437, 146);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.citynameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.regioncityBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(437, 133);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dsBindingSource
+            // 
+            this.dsBindingSource.DataSource = this.ds;
+            this.dsBindingSource.Position = 0;
+            // 
+            // ds
+            // 
+            this.ds.DataSetName = "ds";
+            this.ds.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Населенные пункты: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.regionslistBindingSource;
+            this.comboBox1.DisplayMember = "region_name";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(247, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Список областей: ";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(4, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(355, 31);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(130, 28);
+            this.toolStripButton1.Text = "Обновить улицы";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // regionslistBindingSource
+            // 
+            this.regionslistBindingSource.DataMember = "regions_list";
+            this.regionslistBindingSource.DataSource = this.dsBindingSource;
+            // 
+            // regions_listTableAdapter
+            // 
+            this.regions_listTableAdapter.ClearBeforeFill = true;
+            // 
+            // regionslistBindingSource1
+            // 
+            this.regionslistBindingSource1.DataMember = "regions_list";
+            this.regionslistBindingSource1.DataSource = this.dsBindingSource;
+            // 
+            // regioncityBindingSource
+            // 
+            this.regioncityBindingSource.DataMember = "region_city";
+            this.regioncityBindingSource.DataSource = this.regionslistBindingSource;
+            // 
+            // city_listTableAdapter
+            // 
+            this.city_listTableAdapter.ClearBeforeFill = true;
+            // 
+            // citynameDataGridViewTextBoxColumn
+            // 
+            this.citynameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.citynameDataGridViewTextBoxColumn.DataPropertyName = "city_name";
+            this.citynameDataGridViewTextBoxColumn.HeaderText = "Название населенного пункта";
+            this.citynameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.citynameDataGridViewTextBoxColumn.Name = "citynameDataGridViewTextBoxColumn";
+            // 
+            // streetcityBindingSource
+            // 
+            this.streetcityBindingSource.DataMember = "street_city";
+            this.streetcityBindingSource.DataSource = this.regioncityBindingSource;
+            // 
+            // street_listTableAdapter
+            // 
+            this.street_listTableAdapter.ClearBeforeFill = true;
+            // 
+            // streetnameDataGridViewTextBoxColumn
+            // 
+            this.streetnameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.streetnameDataGridViewTextBoxColumn.DataPropertyName = "street_name";
+            this.streetnameDataGridViewTextBoxColumn.HeaderText = "Название улицы";
+            this.streetnameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.streetnameDataGridViewTextBoxColumn.Name = "streetnameDataGridViewTextBoxColumn";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(173, 28);
+            this.toolStripButton2.Text = "Обновить базу данных";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ds)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.regionslistBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.regionslistBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.regioncityBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.streetcityBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.BindingSource dsBindingSource;
+        private ds ds;
+        private System.Windows.Forms.BindingSource regionslistBindingSource;
+        private dsTableAdapters.regions_listTableAdapter regions_listTableAdapter;
+        private System.Windows.Forms.BindingSource regionslistBindingSource1;
+        private System.Windows.Forms.BindingSource regioncityBindingSource;
+        private dsTableAdapters.city_listTableAdapter city_listTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn citynameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource streetcityBindingSource;
+        private dsTableAdapters.street_listTableAdapter street_listTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn streetnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ Lab9/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Lab9.dsTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                street_listTableAdapter.Update(ds.street_list);
+
+            }
+            catch (DBConcurrencyException ex)
+            {
+                MessageBox.Show("Произошла ошибка при обновлении базы данных на строке " + ex.Row);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при обновлении базы данных: " + ex.Message);
+            }
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "ds.street_list". При необходимости она может быть перемещена или удалена.
+            this.street_listTableAdapter.Fill(this.ds.street_list);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "ds.city_list". При необходимости она может быть перемещена или удалена.
+            this.city_listTableAdapter.Fill(this.ds.city_list);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "ds.regions_list". При необходимости она может быть перемещена или удалена.
+            this.regions_listTableAdapter.Fill(this.ds.regions_list);
+
+        }
+        /*regions->city->street*/
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                ds.street_listDataTable streetDeleted =
+                    (ds.street_listDataTable)ds.street_list.GetChanges(DataRowState.Deleted);
+
+                if (streetDeleted != null)
+                {
+                    street_listTableAdapter.Update(streetDeleted);
+                }
+
+                ds.city_listDataTable cityDeleted =
+                    (ds.city_listDataTable)ds.city_list.GetChanges(DataRowState.Deleted);
+                ds.city_listDataTable cityAdded =
+                    (ds.city_listDataTable)ds.city_list.GetChanges(DataRowState.Added);
+                ds.city_listDataTable cityModified =
+                    (ds.city_listDataTable)ds.city_list.GetChanges(DataRowState.Modified);
+
+                if (cityDeleted != null)
+                {
+                    city_listTableAdapter.Update(cityDeleted);
+                }
+                if (cityAdded != null)
+                {
+                    city_listTableAdapter.Update(cityAdded);
+                }
+                if (cityModified != null)
+                {
+                    city_listTableAdapter.Update(cityModified);
+                }
+
+                ds.street_listDataTable streetAddedOrModified =
+                    (ds.street_listDataTable)ds.street_list.GetChanges(DataRowState.Added | DataRowState.Modified);
+
+                if (streetAddedOrModified != null)
+                {
+                    street_listTableAdapter.Update(streetAddedOrModified);
+                }
+
+                ds.AcceptChanges();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при обновлении базы данных: " + ex.Message);
+            }
+        }
+
+    }
+}
